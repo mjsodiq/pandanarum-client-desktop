2bdbcd0fb1bfcc546a36650e29528587f2a0b79e
MJS <mjsodiq@gmail.com>
2022-08-06 18:56:49 +0700

    inisialization

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React from "react";
 function App() {
     return (
         <div className="App min-h-screen min-w-full flex flex-col">
-            <div className=" self-center border">Under maintenance</div>
+            <div className=" self-center">Under maintenance</div>
         </div>
     );
 }
