b99f3141ba9249f4437e1d723cc4c54627163ddf
MJS <mjsodiq@gmail.com>
2022-08-06 21:04:03 +0700

    configure env

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import React from "react";
 
 function App() {
-    console.log(process.env);
-
     return (
         <div className="App min-h-screen min-w-full flex flex-col">
             <div className=" self-center">Under maintenance</div>
