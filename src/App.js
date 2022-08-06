2e64e6a76a01e5b1d5fbcc89e401ee7a13541bfe
MJS <mjsodiq@gmail.com>
2022-08-06 20:54:54 +0700

    Test env

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from "react";
 
 function App() {
+    console.log(process.env);
+
     return (
         <div className="App min-h-screen min-w-full flex flex-col">
             <div className=" self-center">Under maintenance</div>
