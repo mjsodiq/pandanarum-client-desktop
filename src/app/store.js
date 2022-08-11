2fd7a34991c310f8bc3190334f2ed80dab11e42f
MJS <mjsodiq@gmail.com>
2022-08-12 00:20:42 +0700

    change to typescript

--- src/app/store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import global_slice from "../features/global/global_slice";
-
-export const store = configureStore({
-    reducer: {
-        global_slice: global_slice,
-    },
-});
